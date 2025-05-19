043bdab055735e425a39fab3ce8cae602d5f35fc
DIMFLIX <dimflix.official@gmail.com>
2025-05-19 19:41:57 +0300

    Added a custom handler for systemd to loguru

--- src/mewline/utils/setup_loguru.py
+++ src/mewline/utils/setup_loguru.py
@@ -1,6 +1,16 @@
 from loguru import logger
 from systemd import journal
 
+LOGURU_TO_JOURNALD: dict[str, int] = {
+    "TRACE": journal.Priority.DEBUG,
+    "DEBUG": journal.Priority.DEBUG,
+    "INFO": journal.Priority.INFO,
+    "SUCCESS": journal.Priority.NOTICE,
+    "WARNING": journal.Priority.WARNING,
+    "ERROR": journal.Priority.ERROR,
+    "CRITICAL": journal.Priority.CRITICAL,
+}
+
 
 def disable_logging():
     for log in [
@@ -11,6 +21,33 @@ def disable_logging():
         logger.disable(log)
 
 
+def journald_sink(message):
+    record = message.record
+
+    fields = {
+        "MESSAGE": message,
+        "PRIORITY": LOGURU_TO_JOURNALD.get(record["level"].name, journal.Priority.INFO),
+        "CODE_FILE": record["file"].name,
+        "CODE_LINE": record["line"],
+        "CODE_FUNC": record["function"],
+        "LOGGER": record["name"],
+        "THREAD": f"{record['thread'].name} ({record['thread'].id})",
+    }
+
+    if record["exception"]:
+        fields["EXCEPTION"] = str(record["exception"])
+
+    journal.send(**fields)
+
+
 def setup_loguru() -> None:
     disable_logging()
-    logger.add(journal.JournaldLogHandler("mewline"), level="INFO", format="{message}")
+
+    # кастомный обработчик для systemd
+    logger.add(
+        journald_sink,
+        level="INFO",
+        format="{message}",
+        filter=lambda record: "SUCCESS"
+        not in record["message"],
+    )
