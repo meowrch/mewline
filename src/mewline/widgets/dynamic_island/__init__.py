4817e417f8324ead4f21f7fc788d058d4a5077ae
DIMFLIX <dimflix.official@gmail.com>
2025-06-24 20:21:00 +0300

    Revert "Fixed a critical bug with opening/closing a dynamic island. Fixed animations"
    This reverts commit ebbca0b1599c0bfbeced794bd2df7350003b2fb7.

--- src/mewline/widgets/dynamic_island/__init__.py
+++ src/mewline/widgets/dynamic_island/__init__.py
@@ -3,7 +3,6 @@ from fabric.widgets.box import Box
 from fabric.widgets.centerbox import CenterBox
 from fabric.widgets.stack import Stack
 from fabric.widgets.wayland import WaylandWindow as Window
-from gi.repository import GLib
 
 from mewline.widgets.dynamic_island.app_launcher import AppLauncher
 from mewline.widgets.dynamic_island.base import BaseDiWidget
@@ -62,7 +61,7 @@ class DynamicIsland(Window):
             "emoji": self.emoji,
             "clipboard": self.clipboard,
             "network": self.network,
-            "pawlette-themes": self.pawlette_themes,
+            "pawlette-themes": self.pawlette_themes
         }
         self.current_widget: str | None = None
 
@@ -70,14 +69,10 @@ class DynamicIsland(Window):
             name="dynamic-island-content",
             v_expand=True,
             h_expand=True,
-            transition_type="none",
-            transition_duration=0,
+            transition_type="crossfade",
+            transition_duration=250,
             children=[*self.widgets.values()],
         )
-
-        # Система защиты анимаций
-        self.is_animating = False
-        self.pending_operation = None
 
         ##==> Customizing the hotkeys
         ########################################################
@@ -129,115 +124,57 @@ class DynamicIsland(Window):
             method = getattr(module, method_name)
             method(**kwargs)
             return True
-        return False
-
-    def safe_operation(self, operation, *args):
-        """Выполняет операции с защитой от конфликтов анимации."""
-        if self.is_animating:
-            # Если уже идет анимация, запоминаем последнюю запрошенную операцию
-            self.pending_operation = (operation, args)
-            return
-
-        self.is_animating = True
-        operation(*args)
-
-    def reset_animation(self):
-        """Сбрасывает флаг анимации и выполняет отложенные операции."""
-        self.is_animating = False
-
-        if self.pending_operation:
-            op, args = self.pending_operation
-            self.pending_operation = None
-            self.safe_operation(op, *args)
 
         return False
 
     def close(self):
-        """Закрывает активный виджет и возвращается к компактному режиму."""
-        self.safe_operation(self._perform_close)
-
-    def _perform_close(self):
-        """Фактическая логика закрытия."""
-        # Если нет активного виджета, просто показываем компактный вид
-        if self.current_widget is None:
-            self.stack.set_visible_child(self.compact)
-            # Сбрасываем анимацию сразу
-            GLib.timeout_add(self.stack.get_transition_duration(), self.reset_animation)
-            return
-
-        # Сбрасываем фокус клавиатуры
         self.set_keyboard_mode("none")
 
-        # Вызываем метод закрытия виджета
-        widget = self.widgets[self.current_widget]
-        self.call_module_method_if_exists(widget, "close_widget_from_di")
+        if self.current_widget is not None:
+            self.call_module_method_if_exists(
+                self.widgets[self.current_widget], "close_widget_from_di"
+            )
 
-        # Убираем стили
-        widget.remove_style_class("open")
-        self.stack.remove_style_class(self.current_widget)
-
-        # Переключаем на компактный вид
-        self.stack.set_visible_child(self.compact)
-
-        # Сбрасываем текущий виджет
-        self.current_widget = None
-
-        # Обновляем скрытое состояние
         if self.hidden:
             self.di_box.remove_style_class("hideshow")
             self.di_box.add_style_class("hidden")
 
-        # Сбрасываем флаг анимации после завершения перехода
-        GLib.timeout_add(self.stack.get_transition_duration(), self.reset_animation)
+        for widget in self.widgets.values():
+            widget.remove_style_class("open")
+
+        for style in self.widgets:
+            self.stack.remove_style_class(style)
+
+        self.current_widget = None
+        self.stack.set_visible_child(self.compact)
 
     def open(self, widget: str = "date-notification") -> None:
-        """Открывает указанный виджет в Dynamic Island."""
-        self.safe_operation(self._perform_open, widget)
-
-    def _perform_open(self, widget: str):
-        """Фактическая логика открытия."""
         if widget == "compact":
-            self._perform_close()
+            self.current_widget = None
             return
 
-        if widget not in self.widgets:
-            widget = "date-notification"
-
-        if self.current_widget == widget:
-            # Сбрасываем анимацию сразу
-            GLib.timeout_add(self.stack.get_transition_duration(), self.reset_animation)
-            return
-
-        # Убираем скрытое состояние
         if self.hidden:
             self.di_box.remove_style_class("hidden")
             self.di_box.add_style_class("hideshow")
 
-        # Сбрасываем стили предыдущего виджета
-        if self.current_widget is not None:
-            prev_widget = self.widgets[self.current_widget]
-            prev_widget.remove_style_class("open")
-            self.stack.remove_style_class(self.current_widget)
+        for style, w in self.widgets.items():
+            self.stack.remove_style_class(style)
+            w.remove_style_class("open")
 
-        # Устанавливаем новый виджет
+        if widget not in self.widgets:
+            widget = "date-notification"
+
         self.current_widget = widget
-        new_widget = self.widgets[widget]
 
-        # Применяем стили
-        self.stack.add_style_class(widget)
-        new_widget.add_style_class("open")
-
-        # Показываем виджет
-        self.stack.set_visible_child(new_widget)
-
-        # Управление фокусом клавиатуры
-        if hasattr(new_widget, "focuse_kb") and new_widget.focuse_kb:
+        if self.widgets[widget].focuse_kb:
             self.set_keyboard_mode("exclusive")
-        else:
+
+        self.stack.add_style_class(widget)
+        self.stack.set_visible_child(self.widgets[widget])
+        self.widgets[widget].add_style_class("open")
+        self.call_module_method_if_exists(
+            self.widgets[self.current_widget], "open_widget_from_di"
+        )
+
+        if widget == "notification":
             self.set_keyboard_mode("none")
-
-        # Вызываем метод открытия виджета
-        self.call_module_method_if_exists(new_widget, "open_widget_from_di")
-
-        # Сбрасываем флаг анимации после завершения перехода
-        GLib.timeout_add(self.stack.get_transition_duration(), self.reset_animation)
