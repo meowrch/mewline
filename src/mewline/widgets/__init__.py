64a21ffb66f654a39e9bf0e23ffaa7e2a48b2bc0
DIMFLIX <dimflix.official@gmail.com>
2026-01-05 15:25:47 +0300

    Update widgets init to auto-detect window manager and use appropriate status bar

--- src/mewline/widgets/__init__.py
+++ src/mewline/widgets/__init__.py
@@ -1,8 +1,9 @@
-from fabric.widgets.box import Box
-from fabric.widgets.centerbox import CenterBox
-from fabric.widgets.wayland import WaylandWindow
+"""Widget exports with automatic window manager detection."""
 
-from mewline.utils.hyprland_monitors import HyprlandMonitors
+from loguru import logger
+
+from mewline.utils.window_manager import WindowManager
+from mewline.utils.window_manager import detect_window_manager
 from mewline.widgets.battery import Battery
 from mewline.widgets.bluetooth import Bluetooth
 from mewline.widgets.combined_controls import CombinedControlsButton
@@ -11,59 +12,39 @@ from mewline.widgets.language import LanguageWidget
 from mewline.widgets.network_status import NetworkStatus
 from mewline.widgets.ocr import OCRWidget
 from mewline.widgets.power import PowerButton
+from mewline.widgets.status_bar import BspwmStatusBar
+from mewline.widgets.status_bar import WaylandStatusBar
 from mewline.widgets.system_tray import SystemTray
 from mewline.widgets.workspaces import HyprlandWorkSpacesWidget
 
+# Detect window manager and set StatusBar accordingly
+_wm = detect_window_manager()
 
-class StatusBar(WaylandWindow):
-    """A widget to display the status bar panel."""
+if _wm == WindowManager.BSPWM:
+    logger.info("Using BspwmStatusBar for X11/bspwm")
+    StatusBar = BspwmStatusBar
+elif _wm == WindowManager.HYPRLAND:
+    logger.info("Using WaylandStatusBar for Hyprland")
+    StatusBar = WaylandStatusBar
+else:
+    logger.warning(
+        "Unknown window manager, defaulting to WaylandStatusBar. "
+        "If you're using bspwm, make sure it's running."
+    )
+    StatusBar = WaylandStatusBar
 
-    def __init__(self, **kwargs):
-        self.combined_controls = CombinedControlsButton()
-
-        box = CenterBox(
-            name="panel-inner",
-            start_children=Box(
-                spacing=4,
-                orientation="h",
-                children=[SystemTray(), HyprlandWorkSpacesWidget()],
-            ),
-            center_children=Box(
-                spacing=4,
-                orientation="h",
-                children=None,
-            ),
-            end_children=Box(
-                spacing=4,
-                orientation="h",
-                children=[
-                    OCRWidget(),
-                    Battery(),
-                    self.combined_controls,
-                    LanguageWidget(),
-                    DateTimeWidget(),
-                    Bluetooth(),
-                    NetworkStatus(),
-                    PowerButton(),
-                ],
-            ),
-        )
-
-        WaylandWindow.__init__(
-            self,
-            name="panel",
-            layer="top",
-            anchor="left top right",
-            pass_through=False,
-            monitor=HyprlandMonitors().get_current_gdk_monitor_id(),
-            exclusivity="auto",
-            visible=True,
-            all_visible=False,
-            child=box,
-            **kwargs,
-        )
-
-    def set_osd_widget(self, osd_widget):
-        """Set OSD widget reference for combined controls."""
-        if hasattr(self, 'combined_controls'):
-            self.combined_controls.set_osd_widget(osd_widget)
+__all__ = [
+    "Battery",
+    "Bluetooth",
+    "CombinedControlsButton",
+    "DateTimeWidget",
+    "LanguageWidget",
+    "NetworkStatus",
+    "OCRWidget",
+    "PowerButton",
+    "StatusBar",
+    "SystemTray",
+    "HyprlandWorkSpacesWidget",
+    "BspwmStatusBar",
+    "WaylandStatusBar",
+]
