336fd9ed0d58532678c17b6bbcf160883cbf67d6
DIMFLIX <dimflix.official@gmail.com>
2026-01-07 15:33:26 +0300

    Custom fabric module for bspwm

---
+++ src/mewline/custom_fabric/bspwm/__init__.py
@@ -0,0 +1,13 @@
+from mewline.custom_fabric.bspwm.service import Bspwm
+from mewline.custom_fabric.bspwm.widgets import BspwmActiveWindow
+from mewline.custom_fabric.bspwm.widgets import BspwmLanguage
+from mewline.custom_fabric.bspwm.widgets import BspwmWorkspaces
+from mewline.custom_fabric.bspwm.widgets import get_bspwm_connection
+
+__all__ = [
+    "Bspwm",
+    "BspwmActiveWindow",
+    "BspwmLanguage",
+    "BspwmWorkspaces",
+    "get_bspwm_connection",
+]
